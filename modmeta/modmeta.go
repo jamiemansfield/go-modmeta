cbc1eeb8554eb1cb2e14036d21fee5478b9b2f20
Jamie Mansfield <jmansfield@cadixdev.org>
2020-04-10 02:41:56 +0100

    Implement FindMetadata(file)
    Signed-off-by: Jamie Mansfield <jmansfield@cadixdev.org>

--- modmeta/modmeta.go
+++ modmeta/modmeta.go
@@ -2,6 +2,8 @@
 // from mod binaries.
 package modmeta
 
+import "archive/zip"
+
 // Represents a single mod's metadata.
 type ModMetadata struct {
 	// The mod system/loader the mods uses.
@@ -13,4 +15,37 @@ type ModMetadata struct {
 	Description string
 	URL string
 	Authors string
+}
+
+// FindMetadata attempts to find mod information from a Java binary,
+// looking for metadata from Minecraft Forge.
+func FindMetadata(archive string) ([]*ModMetadata, error) {
+	reader, err := zip.OpenReader(archive)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	var mods []*ModMetadata
+	for _, file := range reader.File {
+		// Minecraft Forge / mcmod.info
+		if file.Name == "mcmod.info" {
+			fc, err := file.Open()
+			if err != nil {
+				return nil, err
+			}
+
+			forgeMods, err := ReadMcModInfo(fc)
+			if err != nil {
+				return nil, err
+			}
+			fc.Close()
+
+			for _, mod := range forgeMods {
+				mods = append(mods, mod.ToModMetadata())
+			}
+		}
+	}
+
+	return mods, nil
 }
